745ba71b68d1afb6428ad6bf4f333d0c2c401f22
Merichu Duncan <chue_3@yahoo.com>
2017-01-21 18:09:56 -0500

    Added few commits

---
+++ VirtualPetProj/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you like Panda? \nType yes or no");
+            string panda = Console.ReadLine();
+            if (panda.ToLower() == "yes")
+            {
+                Console.WriteLine("You belong in an awesome group!");
+
+                Console.WriteLine("Please enter your first name?");
+                string firstName = Console.ReadLine();
+                if (firstName == firstName)
+                {
+                    Console.WriteLine("\n" + firstName + ", meet your new pet: ");
+                    VirtualPet pet1 = new VirtualPet(100, 50);
+                    pet1.DisplayOrithePanda();
+                    pet1.OrithePandaIssue();
+                    OritheMoody();
+
+
+                }
+            }
+        }
+        static void OritheMoody()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Feed him");
+                Console.WriteLine("2. Give him water");
+                Console.WriteLine("3. Pet him");
+                int answer = int.Parse(Console.ReadLine());
+                if (answer == 1)
+                {
+                    Console.WriteLine("Ori the Panda's food level is 100%");
+
+                }
+
+                else if (answer == 2)
+                {
+                    Console.WriteLine("Ori the Panda's water level is 100%");
+
+                }
+                else if (answer == 3)
+                {
+                    Console.WriteLine(" Ori is happy!");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Forget it!");
+                }
+
+                //string[] mood = new string[4];
+                //mood[0] = "1.Feed him";
+                //mood[1] = "2.Give him water";
+                //mood[2] = "3.Pet him";
+                //mood[3] = "4. Give him some air";
+
+            }
+        }
+    }
+}
---
+++ VirtualPetProj/VirtualPet.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProj
+{
+    class VirtualPet
+    {
+        //Fields
+        private string name = "Ori";
+        private string color = "Black/White";
+        private int age = 3;
+        private int energyLevel;
+        private int hungerLevel;
+        private int moodLevel;
+        private int thirstLevel;
+
+        //Properties 
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+        public int Age
+
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+        public int Energy
+        {
+            get { return this.energyLevel; }
+            set { this.energyLevel = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hungerLevel; }
+            set { this.hungerLevel = value; }
+        }
+        public int Mood
+        {
+            get { return this.moodLevel; }
+            set { this.moodLevel = value; }
+        }
+        public int Thirst
+        {
+            get { return this.thirstLevel; }
+            set { this.thirstLevel = value; }
+        }
+        //Constructors
+        public VirtualPet(int hungerLevel, int energyLevel, int moodLevel, int thirstLevel)
+        {
+            this.hungerLevel = hungerLevel;
+            this.energyLevel = energyLevel;
+            this.moodLevel = moodLevel;
+            this.thirstLevel = thirstLevel;
+
+        }
+        public VirtualPet(int hungerLevel, int thirstLevel)
+        {
+            this.energyLevel = 100;
+            this.moodLevel = 10;
+            this.thirstLevel = 10;
+            this.hungerLevel = 10;
+
+        }
+
+
+        //Method
+
+        public void OrithePandaIssue()
+        {
+            int hungerLevel = Hunger;
+            if (Hunger <= 60)
+            {
+
+                Console.WriteLine("Now let's help your pet.. What do you want to do now? Would you like to ");
+            }
+
+
+
+        }
+
+
+
+        public void DisplayOrithePanda()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Ori the Panda's hunger level: " + hungerLevel);
+            Console.WriteLine("Ori the Panda's energy level: " + energyLevel);
+            Console.WriteLine("Ori the Panda's mood level: " + moodLevel);
+            Console.WriteLine("Ori the Panda's thirst level: " + thirstLevel);
+            Console.WriteLine("________________________________________________________");
+
+        }
+
+    }
+}
